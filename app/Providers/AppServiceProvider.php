f8c5bd7bc425674b547ec73490cc50b262233900
Emmszhii <emmanuelmanalo707@gmail.com>
2026-06-09 14:52:51 +0800

    update backend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -23,8 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureDefaults();
-    }
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }else{
+             $this->configureDefaults(); 
+        }
+    }   
 
     /**
      * Configure default behaviors for production-ready applications.
